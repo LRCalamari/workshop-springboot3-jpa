841ac12db94998ca0c4106e1d73f2475d6bfae76
LRCalamari <Lrcalamari.06@gmail.com>
2024-04-22 11:27:36 -0300

    Payment, one-to-one association

--- src/main/java/com/lrcalamari/Course/entities/Order.java
+++ src/main/java/com/lrcalamari/Course/entities/Order.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import com.lrcalamari.Course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,9 +31,11 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -70,10 +74,18 @@ public class Order implements Serializable {
 		}
 	}
 
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
